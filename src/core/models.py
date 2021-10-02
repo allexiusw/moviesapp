9263adf369003064ec6941732ec47aa64fb6c6f2
Allexiusw <william.al1379@gmail.com>
2021-10-01 22:04:37 -0600

    Add base models in the core app

---
+++ src/core/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class Movie(models.Model):
+    '''Define de base structure of the Movie entity'''
+    title = models.CharField(_("Title"), max_length=200)
+    description = models.TextField(_("Description"), max_length=500)
+    stock = models.IntegerField(_("Stock"))
+    rental_price = models.DecimalField(_("Rental Price"), max_digits=6, decimal_places=2)
+    sale_price = models.DecimalField(_("Sale Price"), max_digits=6, decimal_places=2)
+    availability = models.BooleanField(_("Is available"), default=True)
+    created_at = models.DateTimeField(_("Created At"), auto_now_add=True)
+    updated_at = models.DateTimeField(_("Created At"), auto_now=True)
+
+    class Meta:
+        verbose_name = _("Movie")
+        verbose_name_plural = _("Movies")
+        ordering = ('title',)
+
+    def __str__(self):
+        '''Return the representation of each row'''
+        return f'{self.title}'
+
+class MovieImages(models.Model):
+    '''Manage as many images per movie as required'''
+    movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    image = models.ImageField(_("Image"))

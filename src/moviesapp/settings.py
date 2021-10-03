78d509098fd58f472a33c96ee2c0e807e7e2a590
Allexiusw <william.al1379@gmail.com>
2021-10-02 23:06:51 -0600

    Change to token auth using djoser

--- src/moviesapp/settings.py
+++ src/moviesapp/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'djoser',
     'core',
     'api',
@@ -121,10 +122,12 @@ USE_L10N = True
 USE_TZ = True
 
 REST_FRAMEWORK = {
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
     ],
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ),
 }
 
 DJOSER = {

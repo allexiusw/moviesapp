f4f3543a1b8180350f9554e00c45eb901afdb68f
Allexiusw <william.al1379@gmail.com>
2021-10-03 10:24:52 -0600

    Add base model to Serializer Movie

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,7 +1,10 @@
 from rest_framework import serializers
 
+from core.models import Movie
+
 
 class MovieSerializer(serializers.ModelSerializer):
     '''Movie translate models to JSON and perform actions to CRUD op'''
     class Meta:
         fields = ('id', 'title', 'description')
+        model = Movie
--- src/api/views.py
+++ src/api/views.py
@@ -5,5 +5,8 @@ from core.models import Movie
 
 
 class MovieViewSet(viewsets.ModelViewSet):
+    '''Define the HTTP endpoint against the serializer mapping CRUD
+        operations to HTTP verbs, (Create -> POST, Update -> PATCH ...)
+    '''
     serializer_class = MovieSerializer
     queryset = Movie.objects.all()

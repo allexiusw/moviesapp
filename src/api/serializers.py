83f52f627726b7780c4f5162f49e1996ae0b4f8f
Allexiusw <william.al1379@gmail.com>
2021-10-03 10:38:36 -0600

    Add MovieImage serializer to get related images

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -1,10 +1,18 @@
 from rest_framework import serializers
 
-from core.models import Movie
+from core.models import Movie, MovieImage
+
+
+class MovieImageSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = ('id', 'image')
+        model = MovieImage
 
 
 class MovieSerializer(serializers.ModelSerializer):
     '''Movie translate models to JSON and perform actions to CRUD op'''
+    images = MovieImageSerializer(source='movieimage_set', many=True)
+
     class Meta:
-        fields = ('id', 'title', 'description')
+        fields = ('id', 'title', 'description', 'images')
         model = Movie

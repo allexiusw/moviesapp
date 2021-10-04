321e063fb3cb7d48207d9c4124260c4ef8352340
Allexiusw <william.al1379@gmail.com>
2021-10-03 19:38:59 -0600

    Set static path root

--- src/moviesapp/settings.py
+++ src/moviesapp/settings.py
@@ -64,7 +64,7 @@ ROOT_URLCONF = 'moviesapp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -163,8 +163,11 @@ EMAIL_USE_SSL = os.getenv('EMAIL_USE_SSL', 'False').lower() == "true"
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
